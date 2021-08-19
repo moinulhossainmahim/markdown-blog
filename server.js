e3e43030a43e8112575cc83a8016bbd660249129
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-08-19 17:25:03 +0600

    final project

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const articleRouter = require('./routes/articles')
+const Article = require('./models/articles')
 
 mongoose.connect('mongodb://127.0.0.1:27017/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true })
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
-app.set('view engine', 'ejs')
-app.use('/articles', articleRouter)
 app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    const articles = [
-      {
-        title: 'Test title',
-        createdAt: new Date(),
-        description: 'Test description',
-      },
-      {
-        title: 'Test title',
-        createdAt: new Date(),
-        description: 'Test description',
-      },
-    ]
-    res.render('articles/index', {articles: articles})
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', {articles})
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000')
